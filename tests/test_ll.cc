c52c948798168710854746296bc7ef5413d6138c
abhigya wangoo <dnathunder@gmail.com>
2020-10-27 21:49:03 -0700

    Implement Big five tests

--- tests/test_ll.cc
+++ tests/test_ll.cc
@@ -16,7 +16,7 @@ TEST_CASE("Push functionality") {
     LinkedList<int> list;
 
     REQUIRE(list.size() == 0);
-    //REQUIRE(list.empty()); TODO
+    REQUIRE(list.empty()); 
 
     SECTION("Push back one element") {
       list.push_back(1);
@@ -44,7 +44,7 @@ TEST_CASE("Push functionality") {
     LinkedList<int> list;
 
     REQUIRE(list.size() == 0);
-    //REQUIRE(list.empty()); TODO
+    REQUIRE(list.empty());
 
     SECTION("Push forward one element") {
       list.push_front(1);
@@ -66,6 +66,17 @@ TEST_CASE("Push functionality") {
       list.push_back(3);
 
       bool passing_condition = list.size() == 3 && list.front() == -2;
+      REQUIRE(passing_condition);
+    }
+    
+    SECTION("Push Forward non-int elements") {
+      LinkedList<char> char_list;
+      
+      char_list.push_front('a');
+      char_list.push_front('b');
+      char_list.push_front('c');
+
+      bool passing_condition = char_list.size() == 3 && char_list.front() == 'c';
       REQUIRE(passing_condition);
     }
   }
@@ -115,13 +126,22 @@ TEST_CASE("Pop functionality") {
         
         REQUIRE(list.size() == 0);
       }
+      SECTION("Pop back non int values") {
+        LinkedList<char> list_char;
+
+        list_char.push_back('a');
+        list_char.push_front('b');
+        list_char.pop_front();
+        list_char.pop_front();
+
+        REQUIRE(list_char.size() == 0);
+      }
     }
   }
 }
 
 TEST_CASE("Miscellaneous container functionality") { // TODO more of these
   LinkedList<int> list;
-
 
   SECTION("Check if LinkedList is empty") {
     REQUIRE(list.empty());
@@ -155,6 +175,12 @@ TEST_CASE("Miscellaneous container functionality") { // TODO more of these
     REQUIRE(list.size() == 0);
   }
 
+  SECTION("Clear Empty LinkedList") {
+    list.clear();
+
+    REQUIRE(list.size() == 0);
+  }
+
   SECTION("Access front and end value") {
     list.push_front(1);
     list.push_back(2);
@@ -181,7 +207,8 @@ TEST_CASE("Miscellaneous container functionality") { // TODO more of these
     second_list.push_back(2);
     second_list.push_back(3);
     
-    REQUIRE(true);//first_list == second_list); TODO
+    bool condition = first_list == second_list;
+    REQUIRE(condition); 
   }
 
   SECTION("Check if LinkedLists are not equal") {
@@ -195,14 +222,21 @@ TEST_CASE("Miscellaneous container functionality") { // TODO more of these
     second_list.push_back(1);
     second_list.push_back(2);
     second_list.push_back(4);
-
-    REQUIRE(true);//first_list != second_list); TODO 
+  
+    bool success = first_list != second_list;
+    REQUIRE(success); 
   }
 }
 
 TEST_CASE("CS126 Custom functions testing") {
   SECTION("Vector to LinkedList constructor") {
     std::vector<int> values;
+    
+    values.push_back(1);
+    values.push_back(2);
+    values.push_back(3);
+    values.push_back(4);
+    
     LinkedList<int> list = LinkedList(values);
     
     for(size_t i = 0; i < values.size();i++) {
@@ -213,6 +247,10 @@ TEST_CASE("CS126 Custom functions testing") {
     }
     
     REQUIRE(true);
+  }
+  
+  SECTION("Empty Vector to LinkedList") {
+    
   }
   
   SECTION("Remove nth node") {
@@ -256,5 +294,97 @@ TEST_CASE("CS126 Custom functions testing") {
     
     bool condition = list.front() == 0 && list.back() == 6 && list.size() == 4;
     REQUIRE(condition);
+  }
+}
+
+TEST_CASE("Iterator functionality") {
+  SECTION("Iterator") {
+    LinkedList<int> list;
+    bool success;
+
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+
+    LinkedList<int>::iterator it = list.begin();
+    
+    SECTION("Traverse list properly") {
+      while(it != nullptr) {
+        success = *it == list.front();
+        list.pop_front();
+        ++it;
+      }
+      
+      REQUIRE(success);
+    }
+
+    SECTION("Traverse empty list properly") {
+      success = list.size() == 0;
+      
+      while(it != nullptr) {
+        success = *it == list.front();
+        list.pop_front();
+        ++it;
+      }
+
+      REQUIRE(success);
+    }
+    
+  }
+}
+
+TEST_CASE("Big Five") {
+  SECTION("Check Move Constructor") {
+    cs126linkedlist::LinkedList<int> list_;
+
+    list_.push_back(1);
+    list_.push_back(2);
+    list_.push_back(3);
+
+    cs126linkedlist::LinkedList<int> list_2 = cs126linkedlist::LinkedList(std::move(list_));
+    
+    bool success = list_2.size() == 3 && list_.size() == 0;
+    REQUIRE(success);
+  }
+  
+  SECTION("Check Copy Constructor") {
+    cs126linkedlist::LinkedList<int> list_;
+
+    list_.push_back(1);
+    list_.push_back(2);
+    list_.push_back(3);
+
+    cs126linkedlist::LinkedList<int> list_2 = cs126linkedlist::LinkedList(list_);
+
+    bool success = (list_2.size() == 3) && (list_.size() == 3);
+    REQUIRE(success);
+  }
+  
+  SECTION("Check Move Assignment") {
+    cs126linkedlist::LinkedList<int> list_;
+
+    list_.push_back(1);
+    list_.push_back(2);
+    list_.push_back(3);
+
+    cs126linkedlist::LinkedList<int> list_2;
+    list_2 = std::move(list_);
+
+    bool success = (list_2.size() == 3) && (list_.size() == 0);
+    REQUIRE(success);
+  }
+  
+  SECTION("Check Copy Assignment") {
+    cs126linkedlist::LinkedList<int> list_;
+
+    list_.push_back(1);
+    list_.push_back(2);
+    list_.push_back(3);
+
+    cs126linkedlist::LinkedList<int> list_2;
+    list_2 = list_;
+
+    bool success = (list_2.size() == 3) && (list_.size() == 3);
+    REQUIRE(success);
   }
 }
